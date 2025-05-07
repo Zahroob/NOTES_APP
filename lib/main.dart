d8c19ae304ae38307481bff466c11ff2b18e2111
Zahroob <Zahroob.com>
2025-05-07 13:58:54 +0300

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( NotsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NotsApp extends StatelessWidget {
+   NotsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        Loginpage.id: (context) => Loginpage(),
+        // Loginpage.id: (context) => Loginpage(),
       },
       debugShowCheckedModeBanner: false,
-      initialRoute: Loginpage.id,
+      // initialRoute: Loginpage.id,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
